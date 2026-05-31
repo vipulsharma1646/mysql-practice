eebbe949667d06155101666141b9e5c1a4948da6
vipul sharma <vipulsharma1646@gmail.com>
2026-05-31 11:30:51 +0530

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics;
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics
+
+SELECT first_name,last_name
+FROM parks_and_recreation.employee_demographics
+
