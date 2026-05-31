e049998b99cecafa6aba4e8c8ce6aaf6e05f342e
vipul sharma <vipulsharma1646@gmail.com>
2026-05-31 18:18:45 +0530

    Add files via upload

---
+++ having.sql
@@ -0,0 +1,12 @@
+SELECT gender ,AVG(age) 
+FROM employee_demographics
+GROUP BY gender
+HAVING AVG(age) > 40
+
+
+SELECT occupation,AVG(salary)
+FROM employee_salary
+WHERE occupation LIKE '%MANAGER%'
+GROUP BY occupation
+HAVING AVG(salary)>75000
+ 
