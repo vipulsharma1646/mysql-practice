22c8a6799a5d7fc3c49fc177755116df9042671c
vipul sharma <vipulsharma1646@gmail.com>
2026-06-02 22:18:12 +0530

    Add files via upload

---
+++ subquery.sql
@@ -0,0 +1,29 @@
+SELECT *
+FROM employee_demographics 
+WHERE employee_id IN
+                 (SELECT employee_id
+                 FROM employee_salary
+				 WHERE dept_id=1)
+                 
+SELECT first_name,salary,AVG(salary)
+FROM employee_salary
+GROUP BY first_name,salary 
+ 
+SELECT first_name,salary,
+(SELECT AVG(salary)
+FROM employee_salary
+)
+FROM employee_salary
+
+SELECT gender,AVG(age),MAX(age),MIN(age),COUNT(age)
+FROM employee_demographics
+GROUP BY gender
+
+
+SELECT AVG(`MAX(age)`)
+FROM
+(SELECT gender,AVG(age),MAX(age),MIN(age),COUNT(age)
+FROM employee_demographics
+GROUP BY gender) AS ag
+
+
